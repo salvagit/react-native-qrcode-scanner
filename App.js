0d78ce4e66bcd7413c0d86594b7a9540825ae292
salva <s@salva.io>
2018-11-28 02:42:38 -0300

    fix style

--- App.js
+++ App.js
@@ -12,17 +12,17 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  Linking,
+  Dimensions,
 } from 'react-native';
 
 import QRCodeScanner from 'react-native-qrcode-scanner';
+var {height, width} = Dimensions.get('window');
 
 type Props = {};
 export default class App extends Component<Props> {
 
   onSuccess(e) {
     console.log(e.data);
-
   }
 
   render() {
@@ -31,16 +31,28 @@ export default class App extends Component<Props> {
 
         <QRCodeScanner
           onRead={this.onSuccess.bind(this)}
+
           topContent={
             <Text style={styles.centerText}>
               Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
             </Text>
           }
+
           bottomContent={
             <TouchableOpacity style={styles.buttonTouchable}>
-              <Text style={styles.buttonText}>OK. Got it!</Text>
+              {/* <Text style={styles.buttonText}>OK. Got it!</Text> */}
             </TouchableOpacity>
           }
+
+          cameraStyle={{
+            height: height
+          }}
+
+          markerStyle={{
+            height: height / 4
+          }}
+
+          showMarker={true}
         />
 
       </View>
@@ -49,6 +61,10 @@ export default class App extends Component<Props> {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    height: height,
+    flex: 1
+  },
   centerText: {
     flex: 1,
     fontSize: 18,
